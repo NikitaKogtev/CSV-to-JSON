e6a84cd81780abb523b075177573ea1765cdbe71
NikitaKogtev <NikitaKogtev@gmail.com>
2022-06-07 00:02:40 +0700

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,25 +5,34 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
-
         String fileName = "data.csv";
 
         List<Employee> list = parseCSV(columnMapping, fileName);
-
         String json = listToJson(list);
+        writeString(json, "data.json");
 
-        writeString(json);
+        List<Employee> list1 = parseXML("data.xml");
+        String json2 = listToJson(list1);
+        writeString(json2, "data2.json");
+
     }
 
     public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -55,8 +64,8 @@ public class Main {
         return json;
     }
 
-    public static void writeString(String json) {
-        try (FileWriter file = new FileWriter("new_data.json")) {
+    public static void writeString(String json, String fileName) {
+        try (FileWriter file = new FileWriter(fileName)) {
             file.write(json);
             file.flush();
         } catch (IOException e) {
@@ -64,4 +73,63 @@ public class Main {
         }
     }
 
+    public static List<Employee> parseXML(String fileName) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new File(fileName));
+
+            List<Employee> employees = new ArrayList<>();
+
+            Node root = doc.getDocumentElement();
+            NodeList nodeList = root.getChildNodes();
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (Node.ELEMENT_NODE == node.getNodeType()) {
+                    NodeList nodeList1 = node.getChildNodes();
+
+                    long id = 0;
+                    String fistName = "";
+                    String lastName = "";
+                    String country = "";
+                    int age = 0;
+
+                    for (int j = 0; j < nodeList1.getLength(); j++) {
+                        Node node1 = nodeList1.item(j);
+                        if (Node.ELEMENT_NODE == node1.getNodeType()) {
+                            Element employee = (Element) node1;
+
+                            switch (employee.getNodeName()) {
+                                case "id":
+                                    id = Long.parseLong(employee.getTextContent());
+                                    break;
+                                case "firstName":
+                                    fistName = employee.getTextContent();
+                                    break;
+                                case "lastName":
+                                    lastName = employee.getTextContent();
+                                    break;
+                                case "country":
+                                    country = employee.getTextContent();
+                                    break;
+                                case "age":
+                                    age = Integer.parseInt(employee.getTextContent());
+                                    break;
+                            }
+                        }
+                    }
+
+                    Employee emp = new Employee(id, fistName, lastName, country, age);
+                    employees.add(emp);
+
+                }
+            }
+            return employees;
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
+
